d4e5fea43ee458e01ef79bd1d3e54f7f083eb18b
Michael McCool <michael.mccool@intel.com>
2017-11-06 09:58:42 +0900

    trial OCF client

--- examples/bridges/ocf-client/ocf-client.js
+++ examples/bridges/ocf-client/ocf-client.js
@@ -6,12 +6,41 @@ function json_pp(object) {
    return util.inspect(object,{depth: null, colors: true});
 }
 
+// Query Thing Directory to find a set of TDs with "Light" in them
+const request = require('request');
+function get_eps(callback) {
+    request('http://plugfest.thingweb.io:8081/td-lookup/sem?text="Light"', { json: true }, 
+    (err, res, body) => {
+        if (err) { return console.log(err); }
+        callback(Object.keys(body));
+    });
+}
+ 
 // Get TD from OCF-TD generator, try to build interface
-WoT.consumeDescriptionUri("http://192.168.1.127:8091/").then(thing => {
-    thing.getProperty("http://192.168.1.127:8000/api/oic/a/rgbled?di=b17ba04b-d383-4afd-818e-51646ea148c3").then( res => {
-       console.log("READ RGB LED: " + res);
-    }).catch(err => console.error(err));
+function toggle_lights(eps) {
+  for (let i=0; i < eps.length; i++) {
+      let base = "http://plugfest.thingweb.io:8081/td-lookup/ep?ep=";
+      WoT.consumeDescriptionUri(base + eps[i]).then(thing => {
+         // Get current status
+         thing.getProperty("Switch Status").then( state => {
+             console.log("LED State: " + state);
+             // Toggle current status
+             let new_state = !state;
+             thing.setProperty("Switch Status",new_state).then( res => {
+                 console.log("Set LED Result: " + res);
+             }).catch(err => console.error(err));
+         }).catch(err => console.error(err));
+         // pretty-print TD
+         console.log("TD:\n",json_pp(thing.getDescription()));
+      }).catch(err => console.error(err));
+  }
+}
 
-    // pretty-print TD
-    console.log("OCF-TD:\n",json_pp(thing.getDescription()));
-}).catch(err => console.error(err));
+function test(eps) {
+  console.log(eps);
+}
+
+// get_eps(test);
+
+// Toggle all the Lights 
+get_eps(toggle_lights);
